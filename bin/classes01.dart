586e045b5bf7a5a86bfb77e6bc4995ef7d96d432
Sanjar17 <sanjarsaidov173@gmail.com>
2023-05-01 13:57:26 +0500

    adsf

--- bin/classes01.dart
+++ bin/classes01.dart
@@ -1,5 +1,1 @@
-import 'package:classes/classes.dart' as classes;
-
-void main(List<String> arguments) {
-  print('Hello world: ${classes.calculate()}!');
-}
+func() {}
--- test/classes_test.dart
+++ test/classes_test.dart
@@ -1,8 +1,8 @@
-import 'package:classes/classes.dart';
+import '../bin/classes01.dart';
 import 'package:test/test.dart';
 
 void main() {
   test('calculate', () {
-    expect(calculate(), 42);
+    expect(func(), 42);
   });
 }
